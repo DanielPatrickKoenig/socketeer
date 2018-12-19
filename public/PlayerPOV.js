aa6a00c31f202d8c5438490e246955e1faa2fb4f
Dan Koenig <dkoenig@gongos.com>
2018-12-19 09:36:15 -0500

    Signed-off-by: Dan Koenig <dkoenig@gongos.com>

--- public/PlayerPOV.js
+++ public/PlayerPOV.js
@@ -4,33 +4,42 @@ function PlayerPOV(_propIDMatrix){
     var _width = containerElement.getBoundingClientRect().width;
     var _height = containerElement.getBoundingClientRect().height;
     var renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.shadowMap.enabled = true;
     // self.$data.renderer = new self.$data.THREE.WebGLRenderer()
     renderer.setSize(_width, _height);
     containerElement.appendChild(renderer.domElement);
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, _width / _height, 1, 1000);
-    var target = new THREE.Vector3(0, 0, 0);
-    var blankMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide });
+    // var target = new THREE.Vector3(0, 0, 0);
+    // var blankMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.DoubleSide });
+
+
+
 
     var loadedObject;
     // var loader = new THREE.ObjectLoader();
-    var light = new THREE.DirectionalLight(0xffffff, 1);
-    var activeQuestion = -1;
-    light.position.set(50, 0, 100);
-    scene.add(light);
+    // var light = new THREE.DirectionalLight(0xffffff, 1);
+    // var activeQuestion = -1;
+    // light.position.set(4, 0, 4);
+    // scene.add(light);
 
+    
+    var activeQuestion = -1;
+    // light.position.set(4, 0, 4);
+    // var light = new THREE.AmbientLight(0xffffff);
+    // scene.add(light);
 
     var testParams = {
         position:{x:0,y:0,z:0},
         rotation:{x:0,y:0,z:0},
         parts:[
-            {type:0,position:{x:0,y:0,z:0},rotation:{x:0,y:0,z:0},size:{width:300,height:300},texture:'0xcc0000'},
-            {type:0,position:{x:150,y:0,z:0},rotation:{x:0,y:90,z:0},size:{width:300,height:300},texture:'0x0000cc'}
+            // {type:0,position:{x:0,y:0,z:0},rotation:{x:0,y:0,z:0},size:{width:300,height:300},texture:'0xcc0000'},
+            // {type:0,position:{x:150,y:0,z:0},rotation:{x:0,y:90,z:0},size:{width:300,height:300},texture:'0x0000cc'}
         ]
     };
     var testOjbect = new ObjectBuilder(testParams,scene);
 
-
+    addLights();
 
 
     //loader.crossOrigin = '';
@@ -44,8 +53,25 @@ function PlayerPOV(_propIDMatrix){
     // loadModel('models/town2.json',scene,propIDMatrix.BUS_STATION,function(o){
     //     console.log('object loaded');
     // });
-
-
+    function addLights(){
+        var light0 = new THREE.AmbientLight(0x202020);
+        light0.name = 'road';
+        scene.add(light0);
+        var light1 = new THREE.PointLight(0xffffff, 0.5);
+        light1.name = 'road';
+        light1.position.set(-12, 15, 10);
+        scene.add(light1);
+        var light2 = new THREE.DirectionalLight(0xffffff, 0.3);
+        light2.name = 'road';
+        light2.position.set(0, 100, 10);
+        scene.add(light2);
+        var light1helper = new THREE.PointLightHelper(light1, 0.2);
+        light1helper.name = 'road';
+        scene.add(light1helper);
+        var light2helper = new THREE.DirectionalLightHelper(light2, 1);
+        light2helper.name = 'road';
+        scene.add(light2helper);
+    }
 
     function objectIsProp(o){
         var _isProp = false;
@@ -94,7 +120,10 @@ function PlayerPOV(_propIDMatrix){
             else{
                 console.log('added it');
                 var geometry = new THREE.BoxGeometry(1.5, 1, 3);
-                var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+                var material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+                // var material = new THREE.MeshLambertMaterial({
+                //     color: 0xff0000
+                // });
                 var obj = new THREE.Mesh(geometry, material);
                 obj.position.x = -34.202299258964096;
                 obj.position.z = 35.5929460574394;
@@ -103,24 +132,6 @@ function PlayerPOV(_propIDMatrix){
                 obj.name = playerID
                 scene.add(obj);
                 camera.position.y = 2;
-                // createQuestionMarkers()
-                
-                // loader.load('models/car9.txt', function (obj) {
-                //     scene.add(obj);
-                    
-                //     obj.scale.y = -1;
-                //     obj.scale.z = -1;
-                    
-                //     obj.position.x = -34.202299258964096;
-                //     obj.position.z = 35.5929460574394;
-                //     obj.position.y = .6;
-                //     obj.rotation.y = 1.684286204951821;
-                //     camera.position.y = 2;
-                //     obj.name = playerID;
-                    
-                    
-
-                // });
             }
         }
         for(var i = scene.children.length-1;i>=0;i--){
@@ -131,19 +142,28 @@ function PlayerPOV(_propIDMatrix){
     }
 
     this.loadModel = function(modelFile,name,handler){
-        var loader = new THREE.ObjectLoader();
-        loader.load(modelFile, function (obj) {
+        var loader = new THREE.GLTFLoader();
+        loader.load(modelFile, function (g) {
             // scene.add(obj);
-            obj.position.x = -34.202299258964096;
-            obj.position.z = 35.5929460574394;
-            obj.position.y = .6;
-            obj.rotation.y = 1.684286204951821;
-            obj.name = name;
-            scene.add(obj);
+            var loadedObjects = [];
+            for (var i = g.scene.children.length - 1; i >= 0; i--) {
+                var obj = g.scene.children[i];
+                // obj.position.x = -34.202299258964096;
+                // obj.position.z = 35.5929460574394;
+                // obj.position.y = .6;
+                // obj.rotation.y = 1.684286204951821;
+                obj.name = name;
+                scene.add(obj);
+                loadedObjects.push(obj);
+            }
+
+
 
             if(handler != undefined){
-                handler(obj);
+                handler(loadedObjects);
             }
+            // light = new THREE.AmbientLight(0xffffff);
+            // scene.add(light);
             // camera.position.y = 2;
             // obj.name = playerID;
 
